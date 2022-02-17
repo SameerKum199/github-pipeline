39d37889e265c6b27252c5a9d90f8a60c6f1e5e0
SameerKum199 <97933384+SameerKum199@users.noreply.github.com>
2022-02-17 10:56:08 +0530

    Update Transfromation-finalcode.py

--- EmrCode/Transfromation-finalcode.py
+++ EmrCode/Transfromation-finalcode.py
@@ -9,7 +9,7 @@ import boto3
 import pyspark.sql.utils import AnalysisException
 
 
-# Initializing Spark session
+# Initializing Spark sessions
 
 spark = SparkSession.builder.appName("Demo").getOrCreate()
 
